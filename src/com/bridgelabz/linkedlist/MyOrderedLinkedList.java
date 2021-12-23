94470833d23668c7b6403f0ccdea2e2b8016b41d
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-23 13:58:34 +0530

    UPDATED: ordered list

--- src/com/bridgelabz/linkedlist/MyOrderedLinkedList.java
+++ src/com/bridgelabz/linkedlist/MyOrderedLinkedList.java
@@ -3,50 +3,124 @@ package com.bridgelabz.linkedlist;
 public class MyOrderedLinkedList {
     public   INode head;
     public   INode tail;
-
+    static int count =1;
+    INode temp ;
+    
     public MyOrderedLinkedList(){
             this.head = null;
             this.tail = null;
         }
     public void add(INode newNode) {
-        if(this.tail == null){
-            this.tail=newNode;
-        }
-        if(this.head== null){
-            this.head=newNode;
 
-        }else{
-            INode tempNode = this.head;
-            this.head = newNode;
-            this.head.setNext(tempNode);
-         }
-    }
-    public  void addSorted(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
         if (this.head == null) {
             this.head = newNode;
-            this.tail = newNode;
+             this.temp= newNode;
         } else {
             INode tempNode = this.head;
-            if ((tempNode.getKey()).compareTo(newNode.getKey()) > 0) {
-                this.head = newNode;
-                newNode.setNext(tempNode);
-            }
-            INode tempNodeT = this.tail;
-            if ((tempNodeT.getKey()).compareTo(newNode.getKey()) < 0) {
-                tempNodeT.setNext(newNode);
-                this.tail = newNode;
-            }
-            while (tempNode.getNext() != null) {
-                if (((tempNode.getKey()).compareTo(newNode.getKey()) < 0) &&
-                        (tempNode.getNext().getKey().compareTo(newNode.getKey()) > 0)) {
-                    INode tempNode1 = tempNode.getNext();
-                    tempNode.setNext(newNode);
-                    newNode.setNext(tempNode1);
+
+            if (count == 1) {
+                if (this.head.getKey().compareTo(newNode.getKey()) > 0) {
+                    this.head = newNode;
+                    this.head.setNext(tempNode);
+                    count++;
                 }
+            }
+            if (count == 2) {
+                if (this.head.getNext().getKey().compareTo(newNode.getKey()) > 0) {
+                    tempNode.setNext(newNode);
+                    newNode.setNext(temp);
+                } else {
+                    tail.setNext(newNode);
+                    this.tail = newNode;
+                	}
+            	}
+        	}
+
+    	}
+    public void isEmpty(){
+        if(this.head == null){
+            System.out.println("Linked list is Empty");
+        }
+        else{
+            System.out.println("Linked list Not Empty");
+        }
+        }
+     public void append(INode newNode){
+        if(this.head == null){
+            this.head=newNode;
+        }
+        if(this.tail== null){
+            this.tail=newNode;
+        }else{
+            this.tail.setNext(newNode);
+            this.tail=newNode;
+        }
+        }
+     public void insert(INode myNode ,INode newNode){
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+        }
+     public INode pop(){
+        INode tempNode = this.head;
+        tempNode = tempNode.getNext();
+        tempNode.setNext(null);
+        return tail;
+        }
+     public void size(){
+        int counter = 1;
+        INode tempNode = this.head;
+        while(tempNode != this.tail) {
+            if (tempNode.getNext() == null) {
+
+            } else {
                 tempNode = tempNode.getNext();
+                counter++;
+            }
+
+        }
+            System.out.println("Size of current Linked List is: "+counter);
+        }
+     public void search(INode myNode) {
+            INode tempNode = this.head;
+            while(tempNode != null ) {
+                if (myNode.getKey() == tempNode.getKey()) {
+                    System.out.println("Element found :"+myNode.getKey());
+                    break;
+                }
+                tempNode=tempNode.getNext();
             }
         }
-    }
+     public void insertUsingKey(INode myNode,INode value){
+            INode tempNode = this.head;
+            while(tempNode != null ) {
+                if (myNode.getKey() == tempNode.getKey()) {
+                    INode temp =tempNode.getNext();
+                    tempNode.setNext(value);
+                    tempNode = tempNode.getNext();
+                    tempNode.setNext(temp);
+                    break;
+                }
+                tempNode=tempNode.getNext();
+            }
+        }
+     public void remove(INode myNode ){
+            INode tempNode = this.head;
+            while(tempNode != null ) {
+                if (myNode.getKey() == tempNode.getKey()) {
+                    INode temp =tempNode.getNext();
+                    //tempNode= null;
+                    INode demoNode = this.head;
+                    demoNode = demoNode.getNext();
+                    demoNode.setNext(temp);
+                    break;
+                }
+                tempNode=tempNode.getNext();
+            }
+        }
     public  void printMyNodes(){
         StringBuffer myNodes = new StringBuffer("My Nodes:");
         INode tempNode = head;
@@ -62,5 +136,3 @@ public class MyOrderedLinkedList {
     }
 }
 
-
-       
--- src/com/bridgelabz/linkedlist/OrderedLinkedList.java
+++ src/com/bridgelabz/linkedlist/OrderedLinkedList.java
@@ -1,22 +1,19 @@
 package com.bridgelabz.linkedlist;
 
 public class OrderedLinkedList {
-	    public static void sortLinkedList(Integer ...args){
-	        MyOrderedLinkedList myLinkedList = new MyOrderedLinkedList();
-	        for (Integer i:args){
-	            MyNode<Integer> myNode = new MyNode<>(i);
-	            myLinkedList.addSorted(myNode);
-	        }
-	        System.out.print("Ordered Linked list is: ");
-	        myLinkedList.printMyNodes();
-	    }
-	    public static void main(String[] args) {
-	        MyNode<Integer> myFirstNode = new MyNode<>(56);
-	        MyNode<Integer> mySecondNode = new MyNode<>(30);
-	        MyNode<Integer> myThirdNode = new MyNode<>(70);
-	       MyNode<Integer> myFourthNode = new MyNode<>(40);
-	        sortLinkedList(56,30,40,70);
+    public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(40);
 
-	    }
+        MyNode<Integer> myFourthNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.add(myFourthNode);
+
+        myLinkedList.printMyNodes();
+    }
 
 }
