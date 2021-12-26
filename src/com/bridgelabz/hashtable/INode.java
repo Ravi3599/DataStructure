a5e28d131d54c0a40b43b4961e5e4ae887fe863b
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-26 17:25:43 +0530

    ADDED: ability to find frequency of words

---
+++ src/com/bridgelabz/hashtable/INode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hashtable;
+
+
+public interface INode<K extends Comparable> {
+        K getKey();
+        void setKey();
+
+        INode getNext();
+        void setNext(INode<K> tempNode);
+}
