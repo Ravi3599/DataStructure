1af20e24a241487bad9a3f21579e8b928bf1c4ff
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-26 18:03:14 +0530

    UPDATED: ability to search

--- src/com/bridgelabz/binarysearchtree/MyBinaryTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/MyBinaryTreeMain.java
@@ -9,6 +9,7 @@ public class MyBinaryTreeMain {
         myBinaryTree.add(30);
         myBinaryTree.add(70);
 
+        myBinaryTree.searchBST(30);
         int size = myBinaryTree.getSize();
         System.out.println("Size of binary tree is: "+size);
         System.out.println("Binary Tree is :");
